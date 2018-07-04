814b1f96c322220bd0f4878c782c4e63d087499f
Anna Fredriksson <leliloro@gmail.com>
2018-07-04 18:45:02 +0200

    almost done

--- balloon.js
+++ balloon.js
@@ -3,8 +3,9 @@
 function Balloon(){
   this.color = this.generateRandomColor();
   this.positionLeft = this.generateRandomNum();
-  this.className = "balloon";
+  this.className = "balloon moving";
   this.id = "balloon";
+  this.moving = true;
 }
 Balloon.prototype.generateRandomColor = function(){
   return '#'+Math.floor(Math.random()*16777215).toString(16);
--- main.js
+++ main.js
@@ -2,20 +2,21 @@ var startButton = document.getElementById('start-btn');
 var balloonBoard = document.getElementById('balloon-board');
 var balloonBtn = document.getElementById('balloon');
 var balloonInMovement = document.getElementsByClassName('')
+var counterSpan = document.querySelector('#counter span');
+console.log(counterSpan);
+var counter = 0;
 
 function getBallonBoard(){
   balloonBoard.setAttribute('class','balloon-board open');
   createBalloons();
 }
 function clickBalloon(){
-  balloonBtn.addEventListener('click',splashBalloon);
+  balloonBtn.addEventListener('click',moveBalloon);
 }
-function clickBalloonInMovement(){
-  balloonBtn.addEventListener('click',reverseBalloon);
-}
-function splashBalloon(){
+function moveBalloon(){
   var self = this;
   self.setAttribute('class','balloon flyUp');
+  deleteBalloon(self);
 }
 function reverseBalloon(){
   var self = this;
@@ -31,20 +32,23 @@ function addBalloon(){
   var balloonDiv = document.createElement('div');
   balloonDiv.className = balloon.className;
   balloonDiv.id = balloon.id;
-  balloonDiv.setAttribute("style", `background-color: ${balloon.color}; left: ${balloon.positionLeft}%`);
-  balloonDiv.addEventListener('click',splashBalloon);
+  balloonDiv.setAttribute('style', `background-color: ${balloon.color}; left: ${balloon.positionLeft}%`);
+  balloonDiv.addEventListener('click',moveAndDelete);
   balloonBoard.appendChild(balloonDiv);
 }
+function moveAndDelete(e){
+  counter++;
+  counterSpan.innerHTML = counter;
+  var currentBalloon = e.currentTarget;
+  currentBalloon.className += ' splash';
+  var posLeft = currentBalloon.offsetLeft;
 
+  posLeft > 500 ? currentBalloon.setAttribute("style", 'left:-120%') : currentBalloon.setAttribute("style", 'left:120%');
 
-// function(){
-//   var balloon = new Balloon();
-//   var balloonDiv = document.createElement('div');
-//   balloonDiv.className = balloon.className;
-//   balloonDiv.id = balloon.id;
-//   balloonDiv.setAttribute("style", `background-color: ${balloon.color}; left: ${balloon.positionLeft}%`);
-//   balloonBoard.appendChild(balloonDiv);
-// }
-
+  setTimeout(function(){
+    currentBalloon.parentNode.removeChild(currentBalloon);
+  },2000);
+}
 startButton.addEventListener('click',getBallonBoard);
+
 clickBalloon();
